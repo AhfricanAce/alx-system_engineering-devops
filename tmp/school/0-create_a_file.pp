c4f2ae5c9b57fa0a69c55b159ebf31eb61666d96
AhfricanAce <135025414+AhfricanAce@users.noreply.github.com>
2024-07-03 05:52:36 +0100

    Delete tmp directory

--- tmp/school/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-#creating a file in /tmp/school
-file { 'ALX':
-  ensure  => 'present',
-  content => 'I love Puppet',
-  group   => 'www-data',
-  mode    => '0744',
-  owner   => 'www-data',
-  path    => '/tmp/school',
-}
