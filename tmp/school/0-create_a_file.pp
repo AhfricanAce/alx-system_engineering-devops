684e2449c1f73660342a0ae50f5f676a84d9bc98
AhfricanAce <ahfricanace@gmail.com>
2024-07-02 21:16:37 -0700

    0x0A. Configuration management

---
+++ tmp/school/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#creating a file in /tmp/school
+file { 'ALX-CM':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+}
